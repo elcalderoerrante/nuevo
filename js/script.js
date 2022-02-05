590186689c23e2804cfcf770cd466967d88d50fe
Andrea <miltonruiz019@gmail.com>
2022-02-05 16:22:43 -0600

    5 Feb 2022

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ document.getElementById('menu-toggle')
 .addEventListener('click', function(){
   document.body.classList.toggle('nav-open');
 });
+
+$(function() {
+  $(document).click(function (event) {
+    $('.navbar-collapse').collapse('hide');
+  });
+});
