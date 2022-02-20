331799344e1da1b38a3df441a13636ee1b5cf021
Andrea <miltonruiz019@gmail.com>
2022-02-20 15:42:42 -0600

    20 Feb 2022

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ document.getElementById('menu-toggle')
 .addEventListener('click', function(){
   document.body.classList.toggle('nav-open');
 });
+
+
+window.onload = function(){
+    document.getElementById('gsc-i-id1').placeholder = 'Buscar recetas';
+  };
