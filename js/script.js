70cb308d11c364bd0728042f292a8356840caf90
Andrea <miltonruiz019@gmail.com>
2022-02-22 16:02:47 -0600

    22 Feb 2022

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
-//--      Toggler    --//
+//--        Toggler          --//
 document.getElementById('menu-toggle')
 .addEventListener('click', function(){
   document.body.classList.toggle('nav-open');
 });
-
-
+//--  Search bar Placeholder --//
 window.onload = function(){
     document.getElementById('gsc-i-id1').placeholder = 'Buscar recetas';
   };
+//--       Owl Carousel     --//
+
+  jQuery(document).ready(function($) {
+  "use strict";
+  $('#recipe-carousel').owlCarousel( {
+  		loop: true,
+  		center: true,
+  		items: 3,
+  		margin: 30,
+  		autoplay: true,
+  		dots:true,
+      nav:true,
+  		autoplayTimeout: 8500,
+  		smartSpeed: 450,
+      navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+  		responsive: {
+  			0: {
+  				items: 1
+  			},
+  			768: {
+  				items: 2
+  			},
+  			1170: {
+  				items: 3
+  			}
+  		}
+  	});
+  });
